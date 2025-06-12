0e9f091e028b7722ba3036ea425b45370fbcd346
Anasmo1323 <anasmohamed3x2@gmail.com>
2025-06-12 11:21:06 +0300

    build.bat → fix

--- build.bat
+++ build.bat
@@ -7,18 +7,11 @@ set C_SRC_DIR=%BASE_DIR%src\c
 set OUTPUT_DIR=%BASE_DIR%src\python\calculator
 
 :: Compiler settings
-set PYTHON_INCLUDES="C:\Python312\Include"  :: UPDATE THIS PATH
+set PYTHON_INCLUDES="C:\Users\Anas Mohamed\AppData\Local\Programs\Python\Python311\Include"
 set GCC_CMD=gcc -shared -fPIC -I %PYTHON_INCLUDES% -o "%OUTPUT_DIR%\_c_calculator.pyd"
 
 :: Source files
-set C_FILES=(
-    "%C_SRC_DIR%\addition\add.c"
-    "%C_SRC_DIR%\subtraction\sub.c"
-    "%C_SRC_DIR%\multiplication\multi.c"
-    "%C_SRC_DIR%\division\div.c"
-    "%C_SRC_DIR%\ophandler\op_handler.c"
-    "%C_SRC_DIR%\module.c"
-)
+set C_FILES="%C_SRC_DIR%\addition\add.c" "%C_SRC_DIR%\subtraction\sub.c" "%C_SRC_DIR%\multiplication\multi.c" "%C_SRC_DIR%\division\div.c" "%C_SRC_DIR%\ophandler\op_handler.c" "%C_SRC_DIR%\module.c"
 
 :: Build command
 %GCC_CMD% %C_FILES%
